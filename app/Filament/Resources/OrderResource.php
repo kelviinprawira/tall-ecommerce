9f3f026616f48e46e608fcae1466f7a22c35a502
kelviinprawira <kelviinprawira@gmail.com>
2024-06-25 03:26:33 +0700

    membuat AddressRelationManager pada menu order

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -207,7 +207,7 @@ class OrderResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            RelationManagers\AddressRelationManager::class
         ];
     }
 
---
+++ app/Filament/Resources/OrderResource/RelationManagers/AddressRelationManager.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Filament\Resources\OrderResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class AddressRelationManager extends RelationManager
+{
+    protected static string $relationship = 'address';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('first_name')
+                    ->required(),
+                Forms\Components\TextInput::make('last_name')
+                    ->required(),
+                Forms\Components\TextInput::make('phone')
+                    ->tel()
+                    ->maxLength(20)
+                    ->required(),
+                Forms\Components\TextInput::make('city')
+                    ->required(),
+                Forms\Components\TextInput::make('state')
+                    ->required(),
+                Forms\Components\TextInput::make('zip_code')
+                    ->numeric()
+                    ->maxLength(10)
+                    ->required(),
+                Forms\Components\Textarea::make('street_address')
+                    ->columnSpanFull()
+                    ->required()
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('street_address')
+            ->columns([
+                Tables\Columns\TextColumn::make('fullname')
+                    ->label('Full Name'),
+                Tables\Columns\TextColumn::make('phone'),
+                Tables\Columns\TextColumn::make('city'),
+                Tables\Columns\TextColumn::make('state'),
+                Tables\Columns\TextColumn::make('zip_code'),
+                Tables\Columns\TextColumn::make('street_address'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
