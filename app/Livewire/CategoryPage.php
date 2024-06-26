5a29ecd51df3c14a81da98d63a425f3bdded680e
kelviinprawira <kelviinprawira@gmail.com>
2024-06-26 13:06:00 +0700

    menampilkan data brand dan category

--- app/Livewire/CategoryPage.php
+++ app/Livewire/CategoryPage.php
@@ -2,12 +2,16 @@
 
 namespace App\Livewire;
 
+use App\Models\Category;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
 class CategoryPage extends Component
 {
+    #[Title('Category - GEGE STORE')]
     public function render()
     {
-        return view('livewire.category-page');
+        $categories = Category::where('is_active', 1)->get();
+        return view('livewire.category-page', compact('categories'));
     }
 }
--- app/Livewire/HomePage.php
+++ app/Livewire/HomePage.php
@@ -2,12 +2,18 @@
 
 namespace App\Livewire;
 
+use App\Models\Brand;
+use App\Models\Category;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
 class HomePage extends Component
 {
+    #[Title('Home - GEGE STORE')]
     public function render()
     {
-        return view('livewire.home-page');
+        $brands = Brand::where('is_active', 1)->get();
+        $categories = Category::where('is_active', 1)->get();
+        return view('livewire.home-page', compact('brands', 'categories'));
     }
 }
